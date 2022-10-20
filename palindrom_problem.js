7396074bf240c35b8c36a6b50a1a76053a062d8a
avishkushwah <avishkushwah50@gmail.com>
2022-10-20 09:14:27 +0530

    adding prime and palindrome problem

---
+++ palindrom_problem.js
@@ -0,0 +1,13 @@
+let str="madam";
+
+let bag=" ";
+
+for (let i=str.length-1; i>=0; i--){
+    bag=bag+str[i];
+}
+
+if (str==bag){
+    console.log("true");
+} {
+    console.log("false");
+}
---
+++ prime_problem.js
@@ -0,0 +1,13 @@
+let n=13;
+let count=2;
+for (let i=1; i<=n; j++){
+    if (n%i==0){
+        count++;
+    }
+}
+
+if (count==2){
+    console.log("Number is prime");
+} else if {
+    console.log("Number is not prime")
+}
